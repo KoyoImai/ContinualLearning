1616156b31fd6b52834942d0c063dec92b8d6bcc
KoyoImai <imaikoyo0314@gmail.com>
2025-10-02 11:14:32 +0000

    add a new data

--- CIL2/main_linear.py
+++ CIL2/main_linear.py
@@ -29,7 +29,7 @@ def parse_option():
 
     # 基本的な実験設定
     parser.add_argument("--log_name", type=str, default="test")
-    parser.add_argument('--method', type=str, default='cclis', choices=['er', 'co2l', 'cclis', 'prco', 'prco-fimcl'])
+    parser.add_argument('--method', type=str, default='cclis', choices=['er', 'co2l', 'cclis', 'prco', 'prco-fimcl', 'prco-fimclv2'])
 
     # データセット関連
     parser.add_argument('--data_folder', type=str, default='/home/kouyou/Datasets/', help='path to custom dataset')
@@ -185,19 +185,10 @@ def make_setup(opt):
 
         model = SupConResNet(name='resnet18', head='mlp', feat_dim=128, seed=opt.seed, opt=opt)
 
-    elif opt.method in ["prco-fimcl"]:
+    elif opt.method in ["prco-fimcl", "prco-fimclv2"]:
 
         if opt.dataset in ["cifar10", "cifar100", "tiny-imagenet"]:
             from models.resnet_cifar_prco import SupConResNet
-        elif opt.dataset in ["imagenet"]:
-            assert False
-        
-        model = SupConResNet(name='resnet18', head='mlp', feat_dim=128, seed=opt.seed, opt=opt)
-
-    elif opt.method in ["prco-fimcl"]:
-
-        if opt.dataset in ["cifar10", "cifar100", "tiny-imagenet"]:
-            from models.resnet_cifar_prco_fimcl import SupConResNet
         elif opt.dataset in ["imagenet"]:
             assert False
         
--- CIL2/train/__init__.py
+++ CIL2/train/__init__.py
@@ -162,6 +162,15 @@ def eval(model, dataloader, opt):
         write_csv(taskil_acc, opt.result_path, "taskil_acc", opt.target_task, opt.target_epoch)
         write_csv(all_task_accuracies, opt.result_path, "all_task_acc", opt.target_task, opt.target_epoch)
         write_csv(all_task_knn_accuracies, opt.result_path, "all_task_knn_acc", opt.target_task, opt.target_epoch)
+    
+
+    elif opt.method in ["prco-fimcl", "prco-fimclv2"]:
+
+        classil_acc, taskil_acc, all_task_accuracies, all_task_knn_accuracies, all_task_losses, classifier = val_cclis(opt, model, None, linear_loader, val_loader, taskil_loaders, knn_train_loaders, opt.target_epoch)
+        write_csv(classil_acc, opt.result_path, "classil_acc", opt.target_task, opt.target_epoch)
+        write_csv(taskil_acc, opt.result_path, "taskil_acc", opt.target_task, opt.target_epoch)
+        write_csv(all_task_accuracies, opt.result_path, "all_task_acc", opt.target_task, opt.target_epoch)
+        write_csv(all_task_knn_accuracies, opt.result_path, "all_task_knn_acc", opt.target_task, opt.target_epoch)
 
 
 
@@ -244,6 +253,16 @@ def eval4timnet(model, dataloader, opt):
         write_csv(all_task_knn_accuracies, opt.result_path, "all_task_knn_acc", opt.target_task, opt.target_epoch)
 
     elif opt.method in ["prco"]:
+
+        # classil_acc, taskil_acc, all_task_accuracies, all_task_knn_accuracies, all_task_losses, classifier = val_cclis(opt, model, None, linear_loader, val_loader, taskil_loaders, knn_train_loaders, opt.target_epoch)
+        classil_acc, taskil_acc, all_task_accuracies, all_task_knn_accuracies, classifier = val_cclis4timnet(opt, model, None, linear_loader, val_loader, taskil_loaders, knn_train_loaders, opt.target_epoch)
+        write_csv(classil_acc, opt.result_path, "classil_acc", opt.target_task, opt.target_epoch)
+        write_csv(taskil_acc, opt.result_path, "taskil_acc", opt.target_task, opt.target_epoch)
+        write_csv(all_task_accuracies, opt.result_path, "all_task_acc", opt.target_task, opt.target_epoch)
+        write_csv(all_task_knn_accuracies, opt.result_path, "all_task_knn_acc", opt.target_task, opt.target_epoch)
+    
+
+    elif opt.method in ["prco-fimcl", "prco-fimclv2"]:
 
         # classil_acc, taskil_acc, all_task_accuracies, all_task_knn_accuracies, all_task_losses, classifier = val_cclis(opt, model, None, linear_loader, val_loader, taskil_loaders, knn_train_loaders, opt.target_epoch)
         classil_acc, taskil_acc, all_task_accuracies, all_task_knn_accuracies, classifier = val_cclis4timnet(opt, model, None, linear_loader, val_loader, taskil_loaders, knn_train_loaders, opt.target_epoch)
