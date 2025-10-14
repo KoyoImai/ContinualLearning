ddcfa85366f6ab1d72d95220cc1e51b733d4f87d
KoyoImai <imaikoyo0314@gmail.com>
2025-10-14 05:27:15 +0000

    add a new data

--- CIL2/preprocesses/__init__.py
+++ CIL2/preprocesses/__init__.py
@@ -27,6 +27,16 @@ def pre_process(opt, model, model2, dataloader, method_tools):
         preprocess_prco(opt, model, method_tools)
 
         return method_tools, model, model2
+    
+    elif opt.method in ["prco-ema"]:
+
+        preprocess_prco(opt, model, method_tools)
+        model.module.reset_ema_model()
+
+        return method_tools, model, model2
+
+
+    # reset_ema_model
 
     else:
         assert False
